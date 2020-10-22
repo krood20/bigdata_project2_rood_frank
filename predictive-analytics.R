f1b4886dbb1f9747a4eb86e09190997a7f119a0b
Michael Frank <frankm@dev1.mike.com>
2020-10-22 09:42:47 -0400

    Trying to get labeled factor data, it may not be possible

--- predictive-analytics.R
+++ predictive-analytics.R
@@ -1,4 +1,5 @@
 library(here)
+library(GGally)
 
 source("predictive-functions.R")
 
@@ -8,6 +9,19 @@ header <- c('age', 'workclass', 'fnlwgt', 'education', 'education_num',
             'capital_gain', 'capital_loss', 'hours_per_week', 'native_cournty',
             'income')
 census.table <- file_to_table(path, header)
-cenus.labeled <- label_data(census.table)
+smallds <- subset(census.table, select=c('age', 'race', 'sex', 'hours_per_week', 'income'))
+smallds <- subset(census.table, select=c('age', 'income'))
+ggpairs(smallds)
+pairs(smallds)
 
-pairs(cenus.labeled[, c('hours_per_week', 'race')])
+#census.labeled <- label_data(census.table)
+#census.no_lab <- file_to_table(path)
+#census.table$age
+
+#pairs(race ~ hours_per_week, data = census.table[, c('hours_per_week', 'race')])
+#plot(hours_per_week ~ race.factor, data =census.labeled[, c('hours_per_week', 'race')])
+
+#trans.factor <- factor(rep(c("t0", "t1", "t2"), c(4, 1, 2)))
+#trans.factor
+
+#plot(trans.factor)
