d075ae72c0a24753d090285818e63866e100f734
Michael Frank <frankm@dev1.mike.com>
2020-10-25 19:53:27 -0400

    Finished correlation

--- predictive-analytics.R
+++ predictive-analytics.R
@@ -1,27 +1,7 @@
 library(here)
-library(GGally)
 
 source("predictive-functions.R")
 
-path <- here("data", "adult.small")
-header <- c('age', 'workclass', 'fnlwgt', 'education', 'education_num',
-            'marital_status', 'occupation', 'relationship', 'race', 'sex',
-            'capital_gain', 'capital_loss', 'hours_per_week', 'native_cournty',
-            'income')
-census.table <- file_to_table(path, header)
-smallds <- subset(census.table, select=c('age', 'race', 'sex', 'hours_per_week', 'income'))
-smallds <- subset(census.table, select=c('age', 'income'))
-ggpairs(smallds)
-pairs(smallds)
-
-#census.labeled <- label_data(census.table)
-#census.no_lab <- file_to_table(path)
-#census.table$age
-
-#pairs(race ~ hours_per_week, data = census.table[, c('hours_per_week', 'race')])
-#plot(hours_per_week ~ race.factor, data =census.labeled[, c('hours_per_week', 'race')])
-
-#trans.factor <- factor(rep(c("t0", "t1", "t2"), c(4, 1, 2)))
-#trans.factor
-
-#plot(trans.factor)
+path <- here("data", "adult.test")
+census <- create_census(path)
+correlate_census(census, 5)
--- predictive-functions.R
+++ predictive-functions.R
@@ -1,21 +1,48 @@
+library(GGally)
+library(lsr)
 
 file_to_table <- function(path, colnames, header = FALSE) {
   stopifnot(file.exists(path))
-  table <- read.table(path, header=header, sep = ",")
+  table <- read.table(path, header=header, sep = ",", stringsAsFactors = TRUE)
   if(!(header || missing(colnames))){
     colnames(table) <- colnames
   }
   table
 }
 
-label_data <- function(table){
-  sapply(table, function(x){
-    if(is.factor(x)) factor(x) else x
+clean_census <- function(table){
+  table <- subset(table, select=-c(fnlwgt, education_num))
+  table <- lapply(table, function(x) {
+    if(is.factor(x)) as.numeric(x) else x
   })
+  as.data.frame(table)
+}
+
+create_census <- function(path){
+  header <- c('age', 'workclass', 'fnlwgt', 'education', 'education_num',
+              'marital_status', 'occupation', 'relationship', 'race', 'sex',
+              'capital_gain', 'capital_loss', 'hours_per_week', 'native_cournty',
+              'income')
+  census <- file_to_table(path, header)
+  clean_census(census)
+}
+
+correlate_census <- function(frame, num_to_eval){
+  # Create frame of top 5 attributes that influence 'income'
+  factors <- subset(frame, select=-income)
+  cor <- correlate(factors, frame['income'])
+  cor.frame <- as.data.frame(cor)
+  cor.frame <- cor.frame[
+    with(cor.frame, order(income, decreasing =TRUE)), 
+  ]
+  top_cor <- row.names(cor.frame)[1:num_to_eval]
+  top_cor <- append(top_cor, 'income')
+  smallds <- subset(frame, select=top_cor)
+  ggcorr(smallds, label = TRUE, palette = "RdGy")
 }
 
 get_accuracy <- function(predictions, labels) {
-  matching <- Map(function(income, labels) { income %in% labels }, predictions, labels)
-  tf <- unlist(matching, use.names=FALSE)
-  return (sum(tf)/length(tf))
+   matching <- Map(function(income, labels) { income %in% labels }, predictions, labels)
+   tf <- unlist(matching, use.names=FALSE)
+   return (sum(tf)/length(tf))
 }
