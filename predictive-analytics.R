4ee009b859321ecd9f2fdc29667ca420fcd89456
Michael Frank <frankm@dev1.mike.com>
2020-10-26 19:47:04 -0400

    Added histograms

--- predictive-analytics.R
+++ predictive-analytics.R
@@ -4,5 +4,11 @@ source("predictive-functions.R")
 
 path <- here("data", "adult.test")
 census <- create_census(path)
-correlate_census(census, 5)
+census.numerical <- lapply(census, function(x) {
+  if(is.factor(x)) as.numeric(x) else x
+})
+census.numerical <- as.data.frame(census.numerical)
+
+correlate_census(census.numerical, 5)
+plot_census_characteristics(census)
 
--- predictive-functions.R
+++ predictive-functions.R
@@ -3,7 +3,13 @@ library(lsr)
 
 file_to_table <- function(path, colnames, header = FALSE) {
   stopifnot(file.exists(path))
-  table <- read.table(path, header=header, sep = ",", stringsAsFactors = TRUE)
+  table <- read.table(
+    path, 
+    header=header, 
+    sep = ",", 
+    stringsAsFactors = TRUE,
+    strip.white = TRUE
+  )
   if(!(header || missing(colnames))){
     colnames(table) <- colnames
   }
@@ -12,9 +18,6 @@ file_to_table <- function(path, colnames, header = FALSE) {
 
 clean_census <- function(table){
   table <- subset(table, select=-c(fnlwgt, education_num))
-  table <- lapply(table, function(x) {
-    if(is.factor(x)) as.numeric(x) else x
-  })
   as.data.frame(table)
 }
 
@@ -35,10 +38,34 @@ correlate_census <- function(frame, num_to_eval){
   cor.frame <- cor.frame[
     with(cor.frame, order(income, decreasing =TRUE)), 
   ]
+  print(cor.frame['income'])
   top_cor <- row.names(cor.frame)[1:num_to_eval]
+  print("Top correlated")
+  print (top_cor)
+  bottom_cor <- rev(row.names(cor.frame))[1:num_to_eval]
+  print('Bottom correlated')
+  print(bottom_cor)
   top_cor <- append(top_cor, 'income')
   smallds <- subset(frame, select=top_cor)
-  ggcorr(smallds, label = TRUE, palette = "RdGy")
+  ggcorr(smallds, label = TRUE)
+  ggpairs(
+    smallds,
+    lower = list(continuous = wrap("points", alpha = 0.3, size=0.1))
+  )
+}
+
+plot_census_characteristics <- function(census){
+  # Sex vs income
+  ggplot(census) + aes(x = age, group=income, fill=income) + 
+    geom_histogram(binwidth = 1, color = 'black')
+  
+  # Hours/w vs income
+  ggplot(census) + aes(x = hours_per_week, group=income, fill=income) + 
+    geom_histogram(binwidth = 10, color = 'black')
+  
+  # Capital gain vs income
+  ggplot(census) + aes(x=capital_gain, group=income, fill=income) +
+    geom_histogram(bins=10, color='black')
 }
 
 get_accuracy <- function(predictions, labels) {
